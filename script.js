2b16a63927db9290df95de677b4620109356d38f
Ross Teixeira <rosstex@airbears2-10-142-36-127.airbears2.1918.berkeley.edu>
2013-09-28 20:40:16 -0700

    Touchup

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $('.post-button').click(function() {
 	what_the_user_typed = $('textarea').val();
-	if what_the_user_typed.length == 0 || what_the_user_typed.length > 140){
+	if what_the_user_typed.length == 0 || what_the_user_typed.length > 140{
 		alert("You Suck");
 	} 
 	else 
 	{
-	$('#right').prepend('<div class="cell"><p>' + what_the_user_typed + '</p></div>');
+	$('#right').before('<div class="cell"><p>' + what_the_user_typed + '</p></div>');
 	$('textarea').val('');
 });

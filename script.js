10f6f0efd5999a67161fa57e2ae4e22fb62cf67c
Ross Teixeira <rosstex@airbears2-10-142-36-127.airbears2.1918.berkeley.edu>
2013-09-28 22:04:18 -0700

    Javascript Finally

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-$('.post-button').click(function() {
-	what_the_user_typed = $('textarea#post').val();
-	$('#tweet').prepend('<div class="tweetcell"><p>' + what_the_user_typed + '</p></div>');
-	$('textarea#post').val('');
-});
+$(document).ready(function() {
+	$('.post-button').click(function() {
+		what_the_user_typed = $('textarea#post').val();
+		$('div#tweet').prepend('<div class="tweetcell"><p>' + what_the_user_typed + '</p></div>');
+		$('textarea#post').val('');
+	});
+})
+

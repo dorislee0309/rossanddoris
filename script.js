95bc983b6d673c806b285c5930d91dd046867c7f
Ross Teixeira <rosstex@airbears2-10-142-36-127.airbears2.1918.berkeley.edu>
2013-09-28 21:34:32 -0700

    General changes

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $('.post-button').click(function() {
-	what_the_user_typed = $('textarea').val();
-	if what_the_user_typed.length == 0 || what_the_user_typed.length > 140{
-		alert("You Suck");
-	} 
-	else 
-	{
-	$('#right').prepend('<div class="cell"><p>' + what_the_user_typed + '</p></div>');
-	$('textarea').val('');
+	what_the_user_typed = $('textarea#post').val();
+	$('#tweet').prepend('<div class="tweetcell"><p>' + what_the_user_typed + '</p></div>');
+	$('textarea#post').val('');
 });
